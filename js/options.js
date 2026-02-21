fe428623b443ce85e634d96db74507b5a829bdd1
redk1ttyy <lexi@redk1tty.net>
2026-02-20 21:24:56 -0500

    No more uncapitalization

--- js/options.js
+++ js/options.js
@@ -1,6 +1,3 @@
-if (localStorage.getItem("recapitalization_enabled") === "true") {
-  document.getElementById("recapitalization").checked = true;
-}
 if (localStorage.getItem("persona_selected")) {
 	document.getElementById("persona").value = localStorage.getItem("persona_selected");
     document.getElementById("avatar").src = `/assets/avatars/${localStorage.getItem("persona_selected")}.png`;
@@ -11,14 +8,6 @@ if (localStorage.getItem("persona_selected")) {
 	}
     personaStylesheet.href = `/stylesheets/${localStorage.getItem("persona_selected")}.css`;
 }
-
-document.getElementById("recapitalization").addEventListener("change", () => {
-  if (document.getElementById("recapitalization").checked) {
-    localStorage.setItem("recapitalization_enabled", "true");
-  } else {
-    localStorage.removeItem("recapitalization_enabled");
-  }
-});
 document.getElementById("persona").addEventListener("change", () => {
     document.getElementById("avatar").src = `/assets/avatars/${document.getElementById("persona").value}.png`;
 	if (document.getElementById("persona").value == "human") {
@@ -30,4 +19,4 @@ document.getElementById("persona").addEventListener("change", () => {
 	localStorage.setItem("persona_selected", document.getElementById("persona").value);
 });
 
-document.getElementById("personaOption").style.display = "block";
+document.getElementById("options").style.display = "block";
